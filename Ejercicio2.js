5a209cd6f1a75ab1ea031736d49920954d120752
AJG <jgustowski@cetsa.com.ar>
2022-10-27 14:54:10 -0300

    Segundo ejercicio

---
+++ Ejercicio2.js
@@ -0,0 +1,8 @@
+const lista = ['Javier', 45, true, 31/03/1976, {
+    titulo: 'Una vida con proposito',
+    autor: 'Rich Warren',
+    fecha: new Date(20,2,2000),
+    url: "http://vidaconprposito.com"
+}]
+
+console.log(lista);
