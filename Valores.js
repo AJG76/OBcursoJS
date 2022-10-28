4c3b793908eaf4d73a43d57d9e95f734171430ff
AJG <jgustowski@cetsa.com.ar>
2022-10-28 14:16:44 -0300

    Quinto Ejercicio

---
+++ Valores.js
@@ -0,0 +1,21 @@
+
+let altura = 180;
+let alturaMts = 1.80;
+
+let alturaMtsFlot = parseFloat(1.80);
+console.log(alturaMtsFlot)
+
+let result = Number.MIN_VALUE;
+
+
+let valMax  = Number.MAX_VALUE;
+console.log(valMax);
+
+let valMin = Number.MIN_VALUE;
+console.log(valMin);
+
+var resul = ((valMin+1) == valMax);
+console.log(resul);
+
+
+
